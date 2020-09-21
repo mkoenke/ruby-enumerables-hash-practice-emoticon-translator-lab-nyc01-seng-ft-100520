d7ae61b0323356b3eeb483f307b045996730b777
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-21 15:14:54 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,19 @@
 # require modules here
+require "yaml"
 
-def load_library
+def load_library(file_path)
   # code goes here
+  hash = YAML.load_file("file_path")
+
+  hash.each do |emoticon_name, inner_hash|
+    inner_hash.each do |language, translation|
+    end
+  end
+#  hash = {emoticon_name:
+#    {:english => english_translation,
+#    :japanese => japanese_translation
+#    }
+#  }
 end
 
 def get_japanese_emoticon
