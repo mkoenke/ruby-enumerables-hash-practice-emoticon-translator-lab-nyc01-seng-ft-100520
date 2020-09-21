38a3ffce782c2a924a6d62f571aca4562d4cf215
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-21 23:56:24 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -20,7 +20,7 @@ def get_japanese_emoticon(file_path, emoticon_name)
     emoji[key][:english] == emoticon_name
   end
   if emoticon_name
-    emoji[key][:japanese]
+    emoji[emoticon_name][:japanese]
     else
       return "Sorry, that emoticon was not found"
     end
