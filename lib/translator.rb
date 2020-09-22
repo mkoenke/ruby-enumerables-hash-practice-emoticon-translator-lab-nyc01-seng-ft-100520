3b2320ce75100c700de16f616929388f6dfe11b3
Mary Koenke <mkoenke@sas.upenn.edu>
2020-09-22 00:03:41 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -33,7 +33,7 @@ def get_english_meaning(file_path, emoticon_name)
   end
   if emoticon_name
   #  emoji.key(:english)
-    emoji
+    emoji.key
   else
     return "Sorry, that emoticon was not found"
   end
